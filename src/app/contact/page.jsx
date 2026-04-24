adef057f355f97fa6a560afacbdcda094c2a72c8
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-04-24 22:22:47 +0600

    form is added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,90 @@
+"use client";
+
+import { Check } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+
 export default function Contact() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const obj = {
+      email,
+      password,
+    };
+
+    console.log(obj);
+  };
+
   return (
     <div className="container mx-auto">
-        <h1 className="text-5xl font-bold text-center pt-20">This is Contact Page</h1>
+      <Form
+        className="flex w-96 flex-col gap-4 border p-8 rounded-2xl mt-8 mx-auto"
+        onSubmit={onSubmit}
+      >
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input placeholder="john@example.com" />
+          <FieldError />
+        </TextField>
+
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <Input placeholder="Enter your password" />
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
     </div>
-  )
+  );
 }
